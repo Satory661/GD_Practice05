585f909603cd8749ed23f5e6bd01e3106d07af07
Satory661 <gafurovelizar@gmail.com>
2025-10-07 22:57:45 +0500

    My first Unity project with HelloWorld script

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -2,8 +2,24 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-   void Start()
-   {
-       Debug.Log("Hello, GameDev!");
-   }
+    public float jumpForce = 5f; // сила прыжка
+    private Rigidbody rb;
+
+    void Start()
+    {
+        Debug.Log("Hello, GameDev!");
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogWarning("На объекте отсутствует компонент Rigidbody!");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && rb != null)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
 }
